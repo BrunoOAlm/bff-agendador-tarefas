ce93c4f3637a1aa758d5eaf8c5d448f02aa3cad5
Bruno <brunoalm75@gmail.com>
2026-04-02 19:32:54 -0300

    adicionado esqueleto do projeto

---
+++ src/test/java/com/bruno/bff_agendadortarefas/BffAgendadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.bff_agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
